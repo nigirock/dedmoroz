94cb08cf194ebd4f86226f0fc48120f02852ec27
nigirock <nigirock124@gmail.com>
2016-11-07 20:26:22 +0300

    добавил снежинки

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@ $(document).ready(function () {
     $('.parallax-window').parallax1();
     $('#scene').parallax({
         scalarX: 10,
-        scalarY: 10
+        frictionX: 0.1,
+        calibrateY: true
     });
     $('#scene2').parallax({
-        scalarX: 10,
+        scalarX: 20,
         scalarY: 10
     });
 });
