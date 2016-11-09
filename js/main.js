d2d7180e4c4b99e1a232521eee5c98012afdc085
admin <nigirock124@gmail.com>
2016-11-09 14:27:24 +0300

    последний блок

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 $(document).ready(function () {
     $('.parallax-window').parallax1();
+    $("#owl-example").owlCarousel({
+        items : 3,
+        autoPlay : 4000,
+        lazyLoad : true
+      });
     $('#scene').parallax({
         scalarX: 10,
         frictionX: 0.1,
@@ -9,4 +14,12 @@ $(document).ready(function () {
         scalarX: 20,
         scalarY: 10
     });
+    $("#input-id").rating();
+    $("#input-id1").rating({
+        clearButton:'<span></span>'
+    });
+    $('#input-id').on('rating.change', function(event, value, caption) {
+        console.log(value);
+        console.log(caption);
+    })
 });
